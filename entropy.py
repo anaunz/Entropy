8dfc008e1937e1f6d74c0ebce2d6e6234a58efed
anaunz <anaunz@gmail.com>
2019-04-02 21:09:33 +0700

    Added gitignore

--- entropy.py
+++ entropy.py
@@ -25,13 +25,13 @@ def approxEn(series, m):
     phi[i] = c / (len(series) - winSize[i] + 1)
   entropy = abs(phi[0] - phi[1])
   
-  # fp = open('Entropy_Result.txt', 'w+')
-  # string = 'Approximate Entropy\nFor series: ',  ' '.join(str(n) for n in series), '\nPhi1: ', str(phi[0]), ', Phi2: ', str(phi[1]), '\nEntropy: ', str(entropy)
-  # fp.writelines(string)
+  fp = open('Approximate_Entropy.txt', 'a+')
+  string = 'For series: ',  ' '.join(str(n) for n in series), '\nPhi1: ', str(phi[0]), ', Phi2: ', str(phi[1]), '\nEntropy: ', str(entropy), '\n\n'
+  fp.writelines(string)
 
-  print('For series:', series)
-  print('Phi1:', phi[0], ', Phi2:', phi[1])
-  print('Entropy:', entropy)
+  # print('For series:', series)
+  # print('Phi1:', phi[0], ', Phi2:', phi[1])
+  # print('Entropy:', entropy)
 
 def shannonEn(series):
   classify = []
@@ -47,8 +47,12 @@ def shannonEn(series):
   for prob in count:
     entropy += prob / len(series) * math.log(prob / len(series), 2)
   entropy *= -1
-  print('For series:', series)
-  print('Entropy:', entropy)
+  # print('For series:', series)
+  # print('Entropy:', entropy)
+
+  fp = open('Shannon_Entropy.txt', 'a+')
+  string = 'For series: ',  ' '.join(str(n) for n in series), '\nEntropy: ', str(entropy), '\n\n'
+  fp.writelines(string)
 
 def sampleEn(series, m):
   winSize = [m, m + 1]
@@ -73,15 +77,20 @@ def sampleEn(series, m):
       c += temp / (len(series) - winSize[i] + 1)
     phi[i] = c / (len(series) - winSize[i] + 1)
   entropy = math.log(phi[0] / phi[1])
-  print('For series:', series)
-  print('Phi1:', phi[0], ', Phi2:', phi[1])
-  print('Entropy:', entropy)
+
+  fp = open('Sample_Entropy.txt', 'a+')
+  string = 'For series: ',  ' '.join(str(n) for n in series), '\nPhi1: ', str(phi[0]), ', Phi2: ', str(phi[1]), '\nEntropy: ', str(entropy), '\n\n'
+  fp.writelines(string)
+
+  # print('For series:', series)
+  # print('Phi1:', phi[0], ', Phi2:', phi[1])
+  # print('Entropy:', entropy)
 
 series1 = [10,20,10,20,10,20,10,20,10,20,10,20]
 series2 = [10,10,20,10,20,20,20,10,10,20,10,20]
 series3 = ['A','A','A','A','B','B','C','D']
-# approxEn(series1, 1)
-# approxEn(series2, 1)
+approxEn(series1, 1)
+approxEn(series2, 1)
 shannonEn(series3)
 sampleEn(series1, 1)
 sampleEn(series2, 1)

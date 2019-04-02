85088af660625682ead41b773ee39f33d5587180
anaunz <anaunz@gmail.com>
2019-04-02 20:54:05 +0700

    Added Shannon Entropy

--- entropy.py
+++ entropy.py
@@ -24,12 +24,36 @@ def approxEn(series, m):
       c += math.log(temp / (len(series) - winSize[i] + 1), 2)
     phi[i] = c / (len(series) - winSize[i] + 1)
   entropy = abs(phi[0] - phi[1])
+  
+  # fp = open('Entropy_Result.txt', 'w+')
+  # string = 'Approximate Entropy\nFor series: ',  ' '.join(str(n) for n in series), '\nPhi1: ', str(phi[0]), ', Phi2: ', str(phi[1]), '\nEntropy: ', str(entropy)
+  # fp.writelines(string)
+
   print('For series: ', series)
   print('Phi1: ', phi[0], ', Phi2: ', phi[1])
   print('Entropy: ', entropy)
 
+def shannonEn(series):
+  classify = []
+  count = []
+  for number1 in series:
+    if number1 not in classify:
+      classify.append(number1)
+      count.append(0)
+      for number2 in series:
+        if number1 == number2:
+          count[classify.index(number1)] += 1
+  entropy = 0
+  for prob in count:
+    entropy += prob / len(series) * math.log(prob / len(series), 2)
+  entropy *= -1
+  print(entropy)
+
 
 series1 = [10,20,10,20,10,20,10,20,10,20,10,20]
 series2 = [10,10,20,10,20,20,20,10,10,20,10,20]
-approxEn(series1, 1)
-approxEn(series2, 1)
+series3 = ['A','A','A','A','B','B','C','D']
+# approxEn(series1, 1)
+# approxEn(series2, 1)
+shannonEn(series3)
+
